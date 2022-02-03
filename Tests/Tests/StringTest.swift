6b9e52306d2ce932a840a266eca396112199178a
Erica Sadun <erica@ericasadun.com>
2022-02-03 10:37:15 -0700

    Initial commit

---
+++ Tests/Tests/StringTest.swift
@@ -0,0 +1,20 @@
+//  Copyright © 2020 Erica Sadun. All rights reserved.
+
+import XCTest
+import GeneralUtility
+import SwiftMiscUtility
+
+class StringTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testStrings() throws {
+        XCTAssertEqual(" ".trimmed(), "")
+        XCTAssertEqual("a\nb".trimmed(), "a\nb")
+        XCTAssertEqual("a\nb\n".trimmed(), "a\nb")
+    }
+}
